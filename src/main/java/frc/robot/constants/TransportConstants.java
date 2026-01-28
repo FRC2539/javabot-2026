fbcca3fa31efbb6ff5d291ec8ed19e1aa0d93e1f
AlexNunemacher <apullinune@gmail.com>
2026-01-27 20:29:51 -0500

    transporter

---
+++ src/main/java/frc/robot/constants/TransportConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+public final class TransportConstants {
+    private TransportConstants() {}
+
+    //#region Motor
+    public static final int transportMotorId = 20; // replace with motor CAN ID
+    public static final String canBus = "CANivore"; // replace if needed
+    public static final double currentLimit = 40.0; 
+    //#endregion
+}
