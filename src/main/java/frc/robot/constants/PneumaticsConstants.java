47e667e7db7c2607fbeab21092412deb9e76ac85
AlexNunemacher <apullinune@gmail.com>
2026-02-04 19:07:32 -0500

    rasp

---
+++ src/main/java/frc/robot/constants/PneumaticsConstants.java
@@ -0,0 +1,17 @@
+package frc.robot.constants;
+
+public final class PneumaticsConstants {
+
+  private PneumaticsConstants() {}
+
+  public static final int pneumaticHubId = 1;
+
+  public static final int intakeForward = 0;
+  public static final int intakeReverse = 1;
+
+  public static final int raspberryForward = 2;
+  public static final int raspberryReverse = 3;
+
+  public static final int raspberry2Forward = 4;
+  public static final int raspberry2Reverse = 5;
+}
