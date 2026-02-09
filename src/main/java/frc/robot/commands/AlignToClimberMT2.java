066298440da7ea2c52845ba71d5f7f8331b3ad1d
AlexNunemacher <apullinune@gmail.com>
2026-02-09 18:51:07 -0500

    big idiot

--- src/main/java/frc/robot/commands/AlignToClimberMT2.java
+++ src/main/java/frc/robot/commands/AlignToClimberMT2.java
@@ -1,107 +1,107 @@
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
-// import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
-// import com.ctre.phoenix6.swerve.SwerveRequest;
-// import com.ctre.phoenix6.swerve.SwerveRequest.ForwardPerspectiveValue;
-// import edu.wpi.first.math.controller.PIDController;
-// import edu.wpi.first.math.controller.ProfiledPIDController;
-// import edu.wpi.first.math.geometry.Pose2d;
-// import edu.wpi.first.math.geometry.Rotation2d;
-// import edu.wpi.first.math.geometry.Transform2d;
-// import edu.wpi.first.math.kinematics.ChassisSpeeds;
-// import edu.wpi.first.math.trajectory.TrapezoidProfile;
-// import edu.wpi.first.math.util.Units;
-// import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
-// import frc.robot.subsystems.vision.LimelightHelpers;
-// import org.littletonrobotics.junction.AutoLogOutput;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveRequest.ForwardPerspectiveValue;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
+import frc.robot.subsystems.vision.LimelightHelpers;
+import org.littletonrobotics.junction.AutoLogOutput;
 
-// public class AlignToClimberMT2 extends CommandBase {
+public class AlignToClimberMT2 extends CommandBase {
 
-//   private final CommandSwerveDrivetrain drive;
+  private final CommandSwerveDrivetrain drive;
 
-//   private final SwerveRequest.ApplyFieldSpeeds applyFieldSpeeds =
-//       new SwerveRequest.ApplyFieldSpeeds()
-//           .withDriveRequestType(DriveRequestType.Velocity)
-//           .withSteerRequestType(SteerRequestType.MotionMagicExpo)
-//           .withForwardPerspective(ForwardPerspectiveValue.BlueAlliance);
+  private final SwerveRequest.ApplyFieldSpeeds applyFieldSpeeds =
+      new SwerveRequest.ApplyFieldSpeeds()
+          .withDriveRequestType(DriveRequestType.Velocity)
+          .withSteerRequestType(SteerRequestType.MotionMagicExpo)
+          .withForwardPerspective(ForwardPerspectiveValue.BlueAlliance);
 
-//   @AutoLogOutput public Pose2d targetPose;
+  @AutoLogOutput public Pose2d targetPose;
 
-//   private final double xOffset;
-//   private final double yOffset;
-//   private final Rotation2d rotationOffset;
+  private final double xOffset;
+  private final double yOffset;
+  private final Rotation2d rotationOffset;
 
-//   public AlignToClimberMT2(
-//       CommandSwerveDrivetrain drivetrain,
-//       Pose2d climberPose,
-//       double xOffset,
-//       double yOffset,
-//       Rotation2d rotationOffset) {
+  public AlignToClimberMT2(
+      CommandSwerveDrivetrain drivetrain,
+      Pose2d climberPose,
+      double xOffset,
+      double yOffset,
+      Rotation2d rotationOffset) {
 
-//     this.drive = drivetrain;
-//     this.targetPose = climberPose;
-//     this.xOffset = xOffset;
-//     this.yOffset = yOffset;
-//     this.rotationOffset = rotationOffset;
+    this.drive = drivetrain;
+    this.targetPose = climberPose;
+    this.xOffset = xOffset;
+    this.yOffset = yOffset;
+    this.rotationOffset = rotationOffset;
 
-//     // addRequirements(drivetrain);
-//   }
-
-// //   @Override
-// //   public void initialize() {
-
-// //   }
+    // addRequirements(drivetrain);
+  }
 
 //   @Override
-//   public void execute() {
+//   public void initialize() {
 
-//     Pose2d currentPose = new Pose2d();
-
-//     boolean leftHasTarget = LimelightHelpers.getTV("limelight-left");
-//     boolean rightHasTarget = LimelightHelpers.getTV("limelight-right");
-
-//     if (leftHasTarget || rightHasTarget) {
-//       if (leftHasTarget && (!rightHasTarget
-//           || LimelightHelpers.getTA("limelight-left") >=
-// LimelightHelpers.getTA("limelight-right"))) {
-//         currentPose =
-// LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight-left").pose;
-//       } else {
-//         currentPose =
-// LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight-right").pose;
-//       }
-//     }
-
-//     // compute transform from target (climber + offsets) to current pose
-//     Transform2d offsetTransform =
-//         new Transform2d(
-//             currentPose.getTranslation().minus(targetPose.getTranslation()),
-//             currentPose.getRotation().minus(targetPose.getRotation()));
-
-//     double xVel = xController.calculate(offsetTransform.getX());
-//     double yVel = yController.calculate(offsetTransform.getY());
-//     double thetaVel = thetaController.calculate(offsetTransform.getRotation().getRadians());
-
-//     if (!leftHasTarget && !rightHasTarget) {
-//       xVel = 0;
-//       yVel = 0;
-//       thetaVel = 0;
-//     }
-
-//     Rotation2d targetRotation = targetPose.getRotation();
-//     ChassisSpeeds tagRelative = new ChassisSpeeds(xVel, yVel, thetaVel);
-//     ChassisSpeeds fieldRelative = ChassisSpeeds.fromRobotRelativeSpeeds(tagRelative,
-// targetRotation);
-
-//     drive.setControl(applyFieldSpeeds.withSpeeds(fieldRelative));
 //   }
 
-//   @Override
-//   public boolean isFinished() {
-//     boolean finished =
-//         xController.atSetpoint() && yController.atSetpoint() && thetaController.atSetpoint();
+  @Override
+  public void execute() {
 
-//     return finished;
-//   }
-// }
+    Pose2d currentPose = new Pose2d();
+
+    boolean leftHasTarget = LimelightHelpers.getTV("limelight-left");
+    boolean rightHasTarget = LimelightHelpers.getTV("limelight-right");
+
+    if (leftHasTarget || rightHasTarget) {
+      if (leftHasTarget && (!rightHasTarget
+          || LimelightHelpers.getTA("limelight-left") >=
+LimelightHelpers.getTA("limelight-right"))) {
+        currentPose =
+LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight-left").pose;
+      } else {
+        currentPose =
+LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight-right").pose;
+      }
+    }
+
+    // compute transform from target (climber + offsets) to current pose
+    Transform2d offsetTransform =
+        new Transform2d(
+            currentPose.getTranslation().minus(targetPose.getTranslation()),
+            currentPose.getRotation().minus(targetPose.getRotation()));
+
+    double xVel = xController.calculate(offsetTransform.getX());
+    double yVel = yController.calculate(offsetTransform.getY());
+    double thetaVel = thetaController.calculate(offsetTransform.getRotation().getRadians());
+
+    if (!leftHasTarget && !rightHasTarget) {
+      xVel = 0;
+      yVel = 0;
+      thetaVel = 0;
+    }
+
+    Rotation2d targetRotation = targetPose.getRotation();
+    ChassisSpeeds tagRelative = new ChassisSpeeds(xVel, yVel, thetaVel);
+    ChassisSpeeds fieldRelative = ChassisSpeeds.fromRobotRelativeSpeeds(tagRelative,
+targetRotation);
+
+    drive.setControl(applyFieldSpeeds.withSpeeds(fieldRelative));
+  }
+
+  @Override
+  public boolean isFinished() {
+    boolean finished =
+        xController.atSetpoint() && yController.atSetpoint() && thetaController.atSetpoint();
+
+    return finished;
+  }
+}
