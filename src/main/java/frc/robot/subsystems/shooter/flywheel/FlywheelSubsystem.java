788501b0b09e35ff14ef76662e0ae1fa648b1b0b
AlexNunemacher <apullinune@gmail.com>
2026-02-09 20:19:36 -0500

    finished branch - review

--- src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelSubsystem.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems.shooter.flywheel;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import org.littletonrobotics.junction.Logger;
 
 public class FlywheelSubsystem extends SubsystemBase {
