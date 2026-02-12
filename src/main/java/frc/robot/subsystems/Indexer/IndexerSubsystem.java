19d1ed4f9e3e5f8ccce2272a1249db3195243227
James <jamesgormleyrechs@gmail.com>
2026-02-12 18:30:51 -0500

    build error fix

--- src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.indexer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.Indexer.IndexerIOInputsAutoLogged;
 
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
