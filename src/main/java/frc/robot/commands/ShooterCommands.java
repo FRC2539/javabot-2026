19e8a764850e455f8d9ae017c312e33de398d6b3
AlexNunemacher <apullinune@gmail.com>
2026-02-14 14:29:48 -0500

    itteration

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -1,41 +1,41 @@
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import edu.wpi.first.math.geometry.Rotation2d;
-// import edu.wpi.first.wpilibj2.command.Command;
-// import edu.wpi.first.wpilibj2.command.Commands;
-// import frc.robot.subsystems.shooter.flywheel.FlywheelSubsystem;
-// import frc.robot.subsystems.shooter.hood.HoodConstants;
-// import frc.robot.subsystems.shooter.hood.HoodSubsystem;
-// import frc.robot.subsystems.shooter.targeting.TargetingSubsystem;
-// import frc.robot.subsystems.indexer.IndexerSubsystem;
-// import frc.robot.subsystems.kicker.KickerSubsystem; // assuming you have one
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.indexer.IndexerSubsystem;
+import frc.robot.subsystems.shooter.flywheel.FlywheelSubsystem;
+import frc.robot.subsystems.shooter.hood.HoodConstants;
+import frc.robot.subsystems.shooter.hood.HoodSubsystem;
+import frc.robot.subsystems.shooter.targeting.TargetingSubsystem;
 
-// public class ShooterCommands {
+public class ShooterCommands {
 
-//   public static Command shoot(
-//       FlywheelSubsystem flywheel,
-//       HoodSubsystem hood,
-//       IndexerSubsystem indexer,
-//       KickerSubsystem kicker,
-//       TargetingSubsystem targeting) {
+  public static Command shoot(
+      FlywheelSubsystem flywheel,
+      HoodSubsystem hood,
+      IndexerSubsystem indexer,
+      TargetingSubsystem targeting) {
 
-//     return Commands.sequence(
+    return Commands.sequence(
 
-//         Commands.runOnce(() -> {
-//           flywheel.setShooterRPM(targeting.getIdealFlywheelRPM()).schedule();
-//           hood.setHoodAngle(
-//                   Rotation2d.fromRadians(HoodConstants.kMinAngleRad))
-//               .schedule();
-//         }),
-
-//         Commands.waitSeconds(0.3),
+            Commands.runOnce(() -> {
+              flywheel.setShooterRPM(targeting.getIdealFlywheelRPM()).schedule();
+              hood.setHoodAngle(
+                      Rotation2d.fromRadians(HoodConstants.kMinAngleRad))
+                  .schedule();
+            }),
 
 
-//         Commands.parallel(
-//             hood.setHoodAngle(targeting.getIdealHoodAngle()),
-//             indexer.runVoltage(8.0),
-//             kicker.runVoltage(8.0)
-//         )
-//     );
-//   }
-// }
+            Commands.waitSeconds(0.3),
+
+
+            Commands.parallel(
+                hood.setHoodAngle(targeting.getIdealHoodAngle()),
+                indexer.index()
+            )
+        )
+
+        .finallyDo(interrupted -> indexer.stop().schedule());
+  }
+}
