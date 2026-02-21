89287396ac0738cd06a14c3d8d8559a5f851fb1d
AlexNunemacher <apullinune@gmail.com>
2026-02-21 13:36:52 -0500

    index

--- src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
@@ -3,52 +3,47 @@ package frc.robot.subsystems.indexer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
 public class IndexerSubsystem extends SubsystemBase {
 
   private final IndexerIO io;
-  private final IndexerIOInputsAutoLogged inputs = new IndexerIOInputsAutoLogged();
-
-  @AutoLogOutput private Goal goal = Goal.STOP;
+  private final IndexerIOInputsAutoLogged inputs =
+      new IndexerIOInputsAutoLogged();
 
   public IndexerSubsystem(IndexerIO io) {
     this.io = io;
-    setDefaultCommand(setVoltage(9));
+
+    setDefaultCommand(stop());
   }
 
   @Override
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Indexer", inputs);
-
-    // switch (goal) {
-    //   case SHOOT -> io.setVoltage(IndexerConstants.shootVolts.get());
-    //   case REVERSE -> io.setVoltage(IndexerConstants.reverseVolts.get());
-    //   case STOP -> io.setVoltage(IndexerConstants.stopVolts.get());
-    // }
-  }
-
-  public Command stop() {
-    return Commands.run(() -> goal = Goal.STOP, this);
   }
 
   public Command runPositive() {
-    return Commands.run(() -> goal = Goal.SHOOT, this);
+    return Commands.run(
+        () -> io.setVoltage(IndexerConstants.shootVolts.get()),
+        this);
   }
 
   public Command runNegative() {
-    return Commands.run(() -> goal = Goal.REVERSE, this);
+    return Commands.run(
+        () -> io.setVoltage(IndexerConstants.reverseVolts.get()),
+        this);
+  }
+
+  public Command stop() {
+    return Commands.run(
+        () -> io.setVoltage(IndexerConstants.stopVolts.get()),
+        this);
   }
 
   public Command setVoltage(double voltage) {
-    return Commands.run(() -> io.setVoltage(voltage), this);
-  }
-
-  public enum Goal {
-    SHOOT,
-    REVERSE,
-    STOP
+    return Commands.run(
+        () -> io.setVoltage(voltage),
+        this);
   }
 }
