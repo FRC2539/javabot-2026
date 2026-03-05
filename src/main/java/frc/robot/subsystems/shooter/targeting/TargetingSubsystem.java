7a33fc14575ce982ac662647f93a03a6ac50861d
sjsonline <sjsonline@gmail.com>
2026-03-04 19:11:51 -0500

    Tweaks

--- src/main/java/frc/robot/subsystems/shooter/targeting/TargetingSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/targeting/TargetingSubsystem.java
@@ -30,7 +30,7 @@ public class TargetingSubsystem extends SubsystemBase {
   @AutoLogOutput public double targetFlywheelRPS;
 
   @AutoLogOutput public double turretPaddingDeg = 0.0;
-  @AutoLogOutput public double distancePaddingMeters = 0.0;
+  @AutoLogOutput public double hoodPaddingRotations = 0.0;
 
   boolean isFerrying = false;
   CommandSwerveDrivetrain drivetrain;
@@ -75,7 +75,7 @@ public class TargetingSubsystem extends SubsystemBase {
     edu.wpi.first.wpilibj.smartdashboard.SmartDashboard.putNumber(
         "Targeting/Turret Padding", turretPaddingDeg);
     edu.wpi.first.wpilibj.smartdashboard.SmartDashboard.putNumber(
-        "Targeting/Dist Padding", distancePaddingMeters);
+        "Targeting/Dist Padding", hoodPaddingRotations);
 
     Pose2d robotPose = drivetrain.getRobotPose();
     ChassisSpeeds fieldSpeeds = drivetrain.getFieldSpeeds();
@@ -90,7 +90,7 @@ public class TargetingSubsystem extends SubsystemBase {
     return runOnce(
         () -> {
           turretPaddingDeg = 0.0;
-          distancePaddingMeters = 0.0;
+          hoodPaddingRotations = 0.0;
         });
   }
 
@@ -103,91 +103,108 @@ public class TargetingSubsystem extends SubsystemBase {
   }
 
   public Command padtargetdeep() {
-    return runOnce(() -> distancePaddingMeters += 0.1); // Distance is usually meters
+    return runOnce(() -> hoodPaddingRotations += 0.05);
   }
 
   public Command padtargetclose() {
-    return runOnce(() -> distancePaddingMeters -= 0.1);
+    return runOnce(() -> hoodPaddingRotations -= 0.05);
   }
 
   public ShootingParameters calculateShot(
       Pose2d robotPose, ChassisSpeeds fieldSpeeds, Translation2d targetPose, boolean SOTM) {
 
-    // Translation2d filteredRobotVelocity =
-    //     new Translation2d(
-    //         vxFilter.calculate(fieldSpeeds.vxMetersPerSecond),
-    //         vyFilter.calculate(fieldSpeeds.vyMetersPerSecond));
+    //     Translation2d futureTurretPos = robotPose.getTranslation()
+    //           .plus(TurretConstants.turretOffset.rotateBy(robotPose.getRotation()));
+
+    //   Translation2d realDisplacementToHub = targetPose.minus(futureTurretPos);
+    //   realDistance = MathUtil.clamp(realDisplacementToHub.getNorm(), 2.1, 5.122);
+    // // Pose2d robotPose, ChassisSpeeds fieldSpeeds, Translation2d targetPose, boolean SOTM) {
+
+    //   // Translation2d filteredRobotVelocity =
+    //   //     new Translation2d(
+    //   //         vxFilter.calculate(fieldSpeeds.vxMetersPerSecond),
+    //   //         vyFilter.calculate(fieldSpeeds.vyMetersPerSecond));
+
+    //   // Translation2d futureTurretPos =
+    //   //     robotPose
+    //   //         .getTranslation()
+    //   //         .plus(TurretConstants.turretOffset.rotateBy(robotPose.getRotation()));
+    //   // .plus(filteredRobotVelocity.times(TargetingConstants.estimatedShotLatency));
+
+    //   // Translation2d realDisplacementToHub = targetPose.minus(futureTurretPos);
+
+    //   // realDistance = realDisplacementToHub.getNorm();
+
+    //   // System.out.println(realDistance);
+    //   realDistance = MathUtil.clamp(realDistance, 2.1, 5.122);
+    //   // double estimatedFlightTime =
+    // TargetingConstants.hubShotMap.get(realDistance).timeOfFlight();
+
+    //   // Translation2d virtualTarget = targetPose;
+
+    //   // double virtualDistance = realDistance;
+
+    //   // if (SOTM) { // Shooting on the move!
+    //   //   for (int i = 0; i < 5; i++) {
+
+    //   //     virtualTarget = targetPose.minus(filteredRobotVelocity.times(estimatedFlightTime));
+
+    //   //     virtualDistance = futureTurretPos.getDistance(virtualTarget);
+
+    //   //     double newFlightTime = 0;//
+    //   // TargetingConstants.hubShotMap.get(virtualDistance).timeOfFlight();
+
+    //   //     if (Math.abs(newFlightTime - estimatedFlightTime) < 0.03) break;
+    //   //     estimatedFlightTime = newFlightTime;
+    //   //   }
+    //   // }
+
+    //   //Translation2d aimingVector =
+    //   //    realDisplacementToHub; // realDisplacementToHub.minus(futureTurretPos);
+
+    //  // Rotation2d robotRelativeTurretAngle =
+    // aimingVector.getAngle().minus(robotPose.getRotation());
+
+    //   // // double rots = robotRelativeTurretAngle.getRotations();
+    //   // // rots = Math.round(rots * Math.pow(10, 2)) / Math.pow(10, 2);
+
+    //   // ShotSettings mapValues = TargetingConstants.hubShotMap.get(realDistance);
+    //   // if (mapValues == null) {
+    //   //   return calculatedParams; // Return last known good params
+    //   // }
+
+    //   // turretPos = new Pose2d(futureTurretPos, robotRelativeTurretAngle);
+
+    //   // targetTurretAngle = robotRelativeTurretAngle;
+    //   // targetHoodAngle = mapValues.hoodAngle();
+    //   // targetFlywheelRPS = mapValues.wheelRPS();
+    //   // return new ShootingParameters(
+    //   //     robotRelativeTurretAngle, mapValues.hoodAngle(), Math.rint(mapValues.wheelRPS()));
+
+    //   //Pose2d robotPose, ChassisSpeeds fieldSpeeds, Translation2d targetPose, boolean SOTM) {
 
     Translation2d futureTurretPos =
         robotPose
             .getTranslation()
             .plus(TurretConstants.turretOffset.rotateBy(robotPose.getRotation()));
-    // .plus(filteredRobotVelocity.times(TargetingConstants.estimatedShotLatency));
 
     Translation2d realDisplacementToHub = targetPose.minus(futureTurretPos);
+    realDistance = MathUtil.clamp(realDisplacementToHub.getNorm(), 2.1, 5.122);
 
-    realDistance = realDisplacementToHub.getNorm();
-
-    // System.out.println(realDistance);
-    realDistance = MathUtil.clamp(realDistance, 2.1, 5.122);
-    // double estimatedFlightTime = TargetingConstants.hubShotMap.get(realDistance).timeOfFlight();
-
-    // Translation2d virtualTarget = targetPose;
-
-    // double virtualDistance = realDistance;
-
-    // if (SOTM) { // Shooting on the move!
-    //   for (int i = 0; i < 5; i++) {
-
-    //     virtualTarget = targetPose.minus(filteredRobotVelocity.times(estimatedFlightTime));
-
-    //     virtualDistance = futureTurretPos.getDistance(virtualTarget);
-
-    //     double newFlightTime = 0;//
-    // TargetingConstants.hubShotMap.get(virtualDistance).timeOfFlight();
-
-    //     if (Math.abs(newFlightTime - estimatedFlightTime) < 0.03) break;
-    //     estimatedFlightTime = newFlightTime;
-    //   }
-    // }
-
-    Translation2d aimingVector =
-        realDisplacementToHub; // realDisplacementToHub.minus(futureTurretPos);
-
-    Rotation2d robotRelativeTurretAngle = aimingVector.getAngle().minus(robotPose.getRotation());
-
-    // // double rots = robotRelativeTurretAngle.getRotations();
-    // // rots = Math.round(rots * Math.pow(10, 2)) / Math.pow(10, 2);
-
-    // ShotSettings mapValues = TargetingConstants.hubShotMap.get(realDistance);
-    // if (mapValues == null) {
-    //   return calculatedParams; // Return last known good params
-    // }
-
-    // turretPos = new Pose2d(futureTurretPos, robotRelativeTurretAngle);
-
-    // targetTurretAngle = robotRelativeTurretAngle;
-    // targetHoodAngle = mapValues.hoodAngle();
-    // targetFlywheelRPS = mapValues.wheelRPS();
-    // return new ShootingParameters(
-    //     robotRelativeTurretAngle, mapValues.hoodAngle(), Math.rint(mapValues.wheelRPS()));
-
-    double paddedDistance = MathUtil.clamp(realDistance + distancePaddingMeters, 2.1, 5.122);
-    ShotSettings mapValues = TargetingConstants.hubShotMap.get(paddedDistance);
-
+    ShotSettings mapValues = TargetingConstants.hubShotMap.get(realDistance);
     if (mapValues == null) {
       return calculatedParams;
     }
 
-    Rotation2d paddedTurretAngle =
-        robotRelativeTurretAngle.plus(Rotation2d.fromDegrees(turretPaddingDeg));
+    Rotation2d robotRelativeTurretAngle =
+        realDisplacementToHub.getAngle().minus(robotPose.getRotation());
+    targetTurretAngle = robotRelativeTurretAngle.plus(Rotation2d.fromDegrees(turretPaddingDeg));
 
-    turretPos = new Pose2d(futureTurretPos, paddedTurretAngle);
+    targetHoodAngle = mapValues.hoodAngle().plus(Rotation2d.fromRotations(hoodPaddingRotations));
 
-    targetTurretAngle = paddedTurretAngle;
-    targetHoodAngle = mapValues.hoodAngle();
     targetFlywheelRPS = mapValues.wheelRPS();
 
+    turretPos = new Pose2d(futureTurretPos, targetTurretAngle);
     calculatedParams =
         new ShootingParameters(targetTurretAngle, targetHoodAngle, Math.rint(targetFlywheelRPS));
 
